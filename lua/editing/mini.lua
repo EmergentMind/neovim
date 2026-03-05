554f2b8f86e12c8a5b1d8ebf22fd2246111bf6df
emergentmind <emergentmind@users.noreply.github.com>
2026-03-05 09:21:05 -0700

    refactor: enable mini.surround with defaults for now
    Signed-off-by: emergentmind <emergentmind@users.noreply.github.com>

--- lua/editing/mini.lua
+++ lua/editing/mini.lua
@@ -1,25 +1,25 @@
+-- FIXME: investigate other mini tools
 return {
   {
     "mini.nvim",
     auto_enable = true,
     event = "DeferredUIEnter",
     after = function(name)
-      --FIXME: Pick a few of these to start and then TODO the rest for
-      --future self
       require("mini.ai").setup()
       require("mini.comment").setup()
       require("mini.pairs").setup()
-      -- FIXME: this interferes with `s` and needs some tweaking
-      require("mini.surround").setup({
-        add = '<leader>sa',
-        delete = '<leader>sd',
-        find = '<leader>sf',
-        find_left = '<leader>sF',
-        highlight = '<leader>sh',
-        replace = '<leader>sr',
-        suffix_last = '<leader>sl',
-        suffix_next = '<leader>sn',
-      })
-    end
+      -- FIXME: surround interferes with `s` and needs some tweaking
+      -- or get used to using r instead of s for old use
+      require("mini.surround").setup({})
+      --   add = "<leader>sa",
+      --   delete = "<leader>sd",
+      --   find = "<leader>sf",
+      --   find_left = "<leader>sF",
+      --   highlight = "<leader>sh",
+      --   replace = "<leader>sr",
+      --   suffix_last = "<leader>sl",
+      --   suffix_next = "<leader>sn",
+      -- })
+    end,
   },
 }
