0ddd32aa03e6870449af2b4cc80707983fdefea3
emergentmind <emergentmind@users.noreply.github.com>
2026-03-05 09:21:07 -0700

    feat: autocmds for highlight on yank and disable autocomment on enter
    Signed-off-by: emergentmind <emergentmind@users.noreply.github.com>

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -1,1 +1,19 @@
+-- [[ Disable auto comment on enter ]]
+-- See :help formatoptions
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "remove formatoptions",
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
 
+-- [[ Highlight on yank ]]
+-- See `:help vim.hl.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.hl.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
