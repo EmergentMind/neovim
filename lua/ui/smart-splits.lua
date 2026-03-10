fd63edb49ec358da2f8163a039eea7803e6ab0fa
emergentmind <emergentmind@users.noreply.github.com>
2026-03-10 11:38:08 -0600

    refactor(splits): remap <leader> motion to <C> motion for split focus and <leader>- to <C-.> for focus previous

--- lua/ui/smart-splits.lua
+++ lua/ui/smart-splits.lua
@@ -8,18 +8,19 @@ return {
       -- moving between splits
       -- these are already mapped in keymaps
       -- Window navigation
-      vim.keymap.set("n", "<leader>h", require("smart-splits").move_cursor_left)
-      vim.keymap.set("n", "<leader>j", require("smart-splits").move_cursor_down)
-      vim.keymap.set("n", "<leader>k", require("smart-splits").move_cursor_up)
-      vim.keymap.set("n", "<leader>l", require("smart-splits").move_cursor_right)
+      vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+      vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+      vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+      vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+      vim.keymap.set("n", "<C-.>", require("smart-splits").move_cursor_previous)
 
       -- resizing splits
       -- these keymaps will also accept a range,
       -- for example `10<C-h>` will `resize_left` by `(10 * config.default_amount)`
-      vim.keymap.set("n", "<C-h>", require("smart-splits").resize_left)
-      vim.keymap.set("n", "<C-j>", require("smart-splits").resize_down)
-      vim.keymap.set("n", "<C-k>", require("smart-splits").resize_up)
-      vim.keymap.set("n", "<C-l>", require("smart-splits").resize_right)
+      vim.keymap.set("n", "<C-left>", require("smart-splits").resize_left)
+      vim.keymap.set("n", "<C-down>", require("smart-splits").resize_down)
+      vim.keymap.set("n", "<C-up>", require("smart-splits").resize_up)
+      vim.keymap.set("n", "<C-right>", require("smart-splits").resize_right)
       -- swapping buffers between windows
       vim.keymap.set("n", "<C-S-h>", require("smart-splits").swap_buf_left)
       vim.keymap.set("n", "<C-S-j>", require("smart-splits").swap_buf_down)
