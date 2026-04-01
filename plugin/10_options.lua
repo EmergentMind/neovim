f76e5973c6db4e6d947a11ad5e651de79e2f07c0
emergentmind <emergentmind@users.noreply.github.com>
2026-04-01 11:07:32 -0600

    refactor: clean up options coming from introdus

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -4,19 +4,8 @@
 local opt = vim.opt
 local fn = vim.fn
 
--- opt.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
-
--- Decrease update time
--- opt.updatetime = 250
--- opt.timeoutlen = 300
-
 -- ================ General Appearance ========================
 opt.hidden = true
--- show line numbers
--- opt.number = true
--- opt.termguicolors = true
--- Keep signcolumn on by default
--- opt.signcolumn = 'yes'
 -- show relative linenumbers
 opt.relativenumber = true
 -- display status line always
@@ -29,47 +18,23 @@ opt.showmode = true
 opt.autoread = true
 -- Highlight matching braces
 opt.showmatch = true
--- Show current line and column
--- opt.ruler = false
--- No sounds
--- opt.visualbell = true
--- Display tabs and trailing spaces visually
--- opt.list = true
--- opt.listchars = {
---   --tab = '» ',
---   trail = '·',
--- }
-
--- ========= Clipboard ==========
--- Sync yank with system clipboard
--- opt.clipboard = 'unnamedplus'
 
 -- ================ Basic Completion =======================
-opt.wildmode = 'list:longest,list:full' -- for tab completion in : command mode
-opt.wildmenu = true -- enable ctrl-n and ctrl-p to scroll thru matches
+-- FIXME: Disabling the wild options for now since they may be conflicting with other settings
+-- opt.wildmode = 'list:longest,list:full' -- for tab completion in : command mode
+-- opt.wildmenu = true -- enable ctrl-n and ctrl-p to scroll thru matches
 -- stuff to ignore when tab completing
-opt.wildignore =
-  '*.o,*.obj,*~,vim/backups,sass-cache,DS_Store,vendor/rails/**,vendor/cache/**,*.gem,log/**,tmp/**,*.png,*.jpg,*.gif'
--- Set completeopt to have a better completion experience
--- opt.completeopt = 'menu,preview,noselect'
+-- opt.wildignore = '*.o,*.obj,*~,vim/backups,sass-cache,DS_Store,vendor/rails/**,vendor/cache/**,*.gem,log/**,tmp/**,*.png,*.jpg,*.gif'
 
 -- ================ Wrapping and Line Breaks ========================
 opt.wrap = false
 -- Wrap lines at convenient points
 opt.linebreak = true
--- stops line wrapping from being confusing
--- opt.breakindent = true
 
 opt.textwidth = 80
--- Toggle wrap function
--- function ToggleWrap()
---   opt.wrap = not opt.wrap:get()
--- end
 
 -- ================ Indentation ======================
 -- opt.smarttab = true
--- opt.cpoptions:append('I')
--- opt.expandtab = true
 -- opt.smartindent = true
 -- opt.autoindent = true
 -- opt.tabstop = 4
@@ -87,33 +52,18 @@ opt.foldmethod = 'indent'
 -- deepest fold is 3 levels
 opt.foldnestmax = 3
 
--- ================ Splits ============================
--- opt.splitbelow = true
--- opt.splitright = true
-
 -- ================ Scrolling ========================
 -- Minimal number of screen lines to keep above and below the cursor.
--- opt.scrolloff = 10
 opt.sidescrolloff = 15
 opt.sidescroll = 1
 
 -- ================ Search and Replace ========================
--- Preview incremental substitutions in a split
--- opt.inccommand = 'split'
 -- searches incrementally as you type instead of after 'enter'
 opt.incsearch = true
--- highlight search results
--- opt.hlsearch = true
--- search case insensitive
--- opt.ignorecase = true
--- search matters if capital letter
--- opt.smartcase = true
 
 -- ================ Movement ========================
 -- allow backspace in insert mode
 opt.backspace = 'indent,eol,start'
--- enable mouse mode
-opt.mouse = 'a'
 
 -- ========= Cursor =========
 opt.guicursor = 'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,n-v-i:blinkon0'
@@ -127,9 +77,7 @@ opt.directory = '$HOME/.vim/swap//,/tmp//,.'
 
 -- ================ Persistent Undo ==================
 -- Keep undo history across sessions, by storing in file.
--- Only works all the time.
 -- See also lua/plugins/editing/atone.lua
-
 if fn.has('persistent_undo') == 1 then
   local undo_dir = fn.expand('~/.vim/backups')
 
@@ -139,18 +87,7 @@ if fn.has('persistent_undo') == 1 then
   end
 
   opt.undodir = undo_dir
-  -- opt.undofile = true
 end
-
--- vim.g.netrw_liststyle = 0
--- vim.g.netrw_banner = 0
-
--- Default: "ltToOCF"
--- Disable:
--- s - search hit TOP
--- W - written messages
--- I - intro messages
--- vim.opt.shortmess:append('sIW')
 
 if vim.g.neovide then
   -- When using rounded borders lualine/tabs clip
