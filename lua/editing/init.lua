ef24e6f38fd818e91ef1b7956eecc2491a27e9d0
emergentmind <emergentmind@users.noreply.github.com>
2026-04-01 10:55:37 -0600

    refactor: remove plugin configs moved to introdus

--- lua/editing/init.lua
+++ lua/editing/init.lua
@@ -1,10 +1,6 @@
 local MP = ...
 return {
   { import = MP:relpath('atone') },
-  -- { import = MP:relpath('better-n') },
   { import = MP:relpath('cutlass') },
   { import = MP:relpath('mini-comment') },
-  -- { import = MP:relpath('nvim-toggler') },
-  -- { import = MP:relpath('resession') },
-  -- { import = MP:relpath('todo-comments') },
 }
--- lua/ui/init.lua
+++ lua/ui/init.lua
@@ -3,15 +3,12 @@ return {
   -- Primary colorscheme
   { import = MP:relpath('mini-base16') },
 
-  -- { import = MP:relpath('confirm-quit') },
   { import = MP:relpath('hardtime') },
   { import = MP:relpath('highlight-colors') },
   { import = MP:relpath('lualine') },
   { import = MP:relpath('neo-tree') },
-  -- { import = MP:relpath('noice') },
   { import = MP:relpath('notify') },
   { import = MP:relpath('nvim-numbertoggle') },
-  -- { import = MP:relpath('smart-splits') },
   { import = MP:relpath('snacks') },
   { import = MP:relpath('tabby') },
   { import = MP:relpath('toggleterm') },
@@ -19,5 +16,4 @@ return {
   { import = MP:relpath('ufo') },
   { import = MP:relpath('vimade') },
   { import = MP:relpath('which-key') },
-  -- { import = MP:relpath('zen-mode') },
 }
